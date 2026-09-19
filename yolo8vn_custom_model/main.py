05e5df16fc1a0b2c2aefc82f1fbb1a0e59bda4f9
JCS93 <192689701+j12Ch05@users.noreply.github.com>
2026-09-20 00:09:52 +0300

    After the fourth and fifth training
    In the fourth training, the model couldn't distinguish between a man and woman .
    In the fifth training, the model could distinguish a phone from a bottle but if the front screen but not from the back of the phone

--- yolo8vn_custom_model/main.py
+++ yolo8vn_custom_model/main.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import cv2
 import os
 
-model = YOLO(os.path.join(os.path.dirname(__file__), "best2.pt"))
+model = YOLO(os.path.join(os.path.dirname(__file__), "best4.pt"))
 
 cap = cv2.VideoCapture(0)
 
